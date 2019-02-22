87108d197007e57ba07547233e4f574d69bcf27a
Kristian Babulev <kriso4os@abv.bg>
2019-02-22 11:23:42 +0200

    Add project files.

---
+++ Quacker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Quacker.Startup))]
+namespace Quacker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
