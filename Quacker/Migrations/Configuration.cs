8d13ccd96f2a95cc0aae444d4842c44dd99745c3
Kristian Babulev <kriso4os@abv.bg>
2019-03-08 16:25:54 +0200

    added first and last name.

---
+++ Quacker/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace Quacker.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Quacker.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Quacker.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(Quacker.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
